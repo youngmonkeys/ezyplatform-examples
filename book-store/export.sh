53b32d799166cbdd6ccc11f89dddfe601ac2bc80
Dung Ta Van <itprono3@gmail.com>
2024-06-16 22:38:26 +0700

    update

--- book-store/export.sh
+++ book-store/export.sh
@@ -16,10 +16,11 @@
 
 set -e
 mvn -pl . clean install
+mvn -pl book-store-sdk clean install
 mvn -pl book-store-admin-plugin clean install -Pexport,\!test
 mvn -pl book-store-socket-app clean install -Pexport,\!test
-mvn -pl book-store-socket-plugin clean install -Pexport,\!test
 mvn -pl book-store-web-plugin clean install -Pexport,\!test
+mvn -pl book-store-socket-plugin clean install -Pexport,\!test
 mvn -pl book-store-theme clean install -Pexport,\!test
 ezy.sh package
 ezy.sh export
