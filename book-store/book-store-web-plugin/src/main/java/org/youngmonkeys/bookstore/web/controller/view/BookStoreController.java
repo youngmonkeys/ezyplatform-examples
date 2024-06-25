b32d18e5174ab26426580f9fda54508fb471e9ec
toilahtc96 <toilahtc@gmail.com>
2024-06-25 09:46:54 +0700

    test menu

--- book-store/book-store-web-plugin/src/main/java/org/youngmonkeys/bookstore/web/controller/view/BookStoreController.java
+++ book-store/book-store-web-plugin/src/main/java/org/youngmonkeys/bookstore/web/controller/view/BookStoreController.java
@@ -4,15 +4,23 @@ import com.tvd12.ezyhttp.server.core.annotation.DoGet;
 import com.tvd12.ezyhttp.server.core.annotation.RequestParam;
 import com.tvd12.ezyhttp.server.core.view.View;
 import lombok.AllArgsConstructor;
+import org.youngmonkeys.bookstore.web.service.ProductCategoryService;
+import org.youngmonkeys.ecommerce.entity.ProductCategory;
+import org.youngmonkeys.ecommerce.entity.ProductCategoryParent;
+import org.youngmonkeys.ecommerce.repo.ProductCategoryParentRepository;
+import org.youngmonkeys.ecommerce.repo.ProductCategoryRepository;
+import org.youngmonkeys.ecommerce.service.ProductCategoryParentService;
 import org.youngmonkeys.ezyarticle.web.manager.WebPageFragmentManager;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 public class BookStoreController {
 
     private final WebPageFragmentManager pageFragmentManager;
-
+    private final ProductCategoryService myProductCategoryService;
     @DoGet("/store")
     public View storeGet(@RequestParam("lang") String language) {
         return View.builder()
@@ -27,8 +35,7 @@ public class BookStoreController {
                 )
                 .addVariable(
                         "categories",
-                        Arrays.asList("cate test 1", "cate test 2", "cate test 3") // mockup data
-                )
-                .build();
+                        myProductCategoryService.getListCategoryMenuItems()
+                ).build();
     }
 }
