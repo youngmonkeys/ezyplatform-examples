e1cf6a206e92757f23dcf54e261f9730483d8d70
toilahtc96 <toilahtc@gmail.com>
2024-07-01 14:37:26 +0700

    update code get category

--- book-store/book-store-web-plugin/src/main/java/org/youngmonkeys/bookstore/web/service/ProductCategoryService.java
+++ book-store/book-store-web-plugin/src/main/java/org/youngmonkeys/bookstore/web/service/ProductCategoryService.java
@@ -7,7 +7,6 @@ import org.youngmonkeys.bookstore.web.repository.ProductCategoryRepository;
 import org.youngmonkeys.ecommerce.entity.ProductCategory;
 import org.youngmonkeys.ecommerce.entity.ProductCategoryParent;
 import org.youngmonkeys.ecommerce.repo.ProductCategoryParentRepository;
-import org.youngmonkeys.ecommerce.service.ProductCategoryParentService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,44 +17,39 @@ import java.util.stream.Collectors;
 public class ProductCategoryService {
 
     private final ProductCategoryParentRepository productCategoryParentRepository;
-    private final ProductCategoryRepository myProductCategoryService;
-    private final ProductCategoryParentService productCategoryParentService;
+    private final ProductCategoryRepository productCategoryService;
 
     public List<CategoryMenuItem> getListCategoryMenuItems() {
         List<CategoryMenuItem> menuItems = new ArrayList<>();
-        List<Long> categoryParentIds = productCategoryParentRepository
-                .findAll()
-                .stream()
-                .map(ProductCategoryParent::getCategoryId)
-                .collect(Collectors.toList());
+        List<Long> categoryParentIds = productCategoryParentRepository.findAll().stream().map(ProductCategoryParent::getCategoryId).collect(Collectors.toList());
 
-        List<ProductCategory> firstGenerationCategories = myProductCategoryService.findByIdNotIn(categoryParentIds);
-        firstGenerationCategories.forEach(
-                first -> {
-                    CategoryMenuItem menuItem = new CategoryMenuItem();
-                    menuItem.setId(first.getId());
-                    menuItem.setDisplayName(first.getName());
-                    menuItem.setDisplayOrder(first.getDisplayOrder());
-                    menuItem.setMenuItems(getChildMenu(first.getId()));
-                    menuItems.add(menuItem);
-                }
-        );
+        List<ProductCategory> allCategories = productCategoryService.findAll();
+        List<ProductCategoryParent> productCategoryParents = productCategoryParentRepository.findAll();
+        allCategories.stream().filter(category -> !categoryParentIds.contains(category.getId())).forEach(category -> {
+            CategoryMenuItem categoryMenuItem = new CategoryMenuItem();
+            categoryMenuItem.setId(category.getId());
+            categoryMenuItem.setDisplayName(category.getDisplayName());
+            categoryMenuItem.setDisplayOrder(category.getDisplayOrder());
+            categoryMenuItem.setMenuItems(getChildMenu(category, productCategoryParents));
+            menuItems.add(categoryMenuItem);
+        });
         return menuItems;
     }
 
-    private List<CategoryMenuItem> getChildMenu(long parentId) {
-        List<CategoryMenuItem> menuItems = new ArrayList<>();
-        productCategoryParentService.getChildIdsByCategoryId(parentId).forEach(
-                child -> {
-                    ProductCategory productCategory = myProductCategoryService.findById(child);
-                    CategoryMenuItem menuItem = new CategoryMenuItem();
-                    menuItem.setId(productCategory.getId());
-                    menuItem.setDisplayName(productCategory.getName());
-                    menuItem.setDisplayOrder(productCategory.getDisplayOrder());
-                    menuItem.setMenuItems(getChildMenu(productCategory.getId()));
-                    menuItems.add(menuItem);
-                }
-        );
-        return menuItems;
+    public List<CategoryMenuItem> getChildMenu(ProductCategory productCategory, List<ProductCategoryParent> productCategoryParents) {
+        List<CategoryMenuItem> childMenu = new ArrayList<>();
+
+        productCategoryParents.forEach(parentId -> {
+            if (parentId.getParentId() == productCategory.getId()) {
+                CategoryMenuItem menuItem = new CategoryMenuItem();
+                ProductCategory childOfThisParent = productCategoryService.findById(parentId.getCategoryId());
+                menuItem.setId(childOfThisParent.getId());
+                menuItem.setDisplayName(childOfThisParent.getDisplayName());
+                menuItem.setDisplayOrder(childOfThisParent.getDisplayOrder());
+                menuItem.setMenuItems(getChildMenu(childOfThisParent, productCategoryParents));
+                childMenu.add(menuItem);
+            }
+        });
+        return childMenu;
     }
 }
