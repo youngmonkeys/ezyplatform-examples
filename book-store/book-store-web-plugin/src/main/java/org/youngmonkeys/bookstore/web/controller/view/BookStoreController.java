436e7e5acd56571c66a81eed17bcab6a715a09d7
toilahtc96 <toilahtc@gmail.com>
2024-07-30 15:42:59 +0700

    update show product and modal

--- book-store/book-store-web-plugin/src/main/java/org/youngmonkeys/bookstore/web/controller/view/BookStoreController.java
+++ book-store/book-store-web-plugin/src/main/java/org/youngmonkeys/bookstore/web/controller/view/BookStoreController.java
@@ -10,6 +10,7 @@ import org.youngmonkeys.ecommerce.entity.ProductCategoryStatus;
 import org.youngmonkeys.ecommerce.model.ProductCurrencyModel;
 import org.youngmonkeys.ecommerce.pagination.DefaultProductFilter;
 import org.youngmonkeys.ecommerce.pagination.DefaultProductPriceFilter;
+import org.youngmonkeys.ecommerce.response.ProductResponse;
 import org.youngmonkeys.ecommerce.service.ProductCurrencyService;
 import org.youngmonkeys.ecommerce.web.controller.service.WebProductCategoryControllerService;
 import org.youngmonkeys.ecommerce.web.controller.service.WebProductControllerService;
@@ -20,21 +21,23 @@ import java.util.Collections;
 @AllArgsConstructor
 public class BookStoreController {
 
-    private final WebProductCategoryControllerService productCategoryControllerService;
+    private final WebProductCategoryControllerService webProductCategoryControllerService;
     private final WebProductControllerService webProductControllerService;
     private final ProductCurrencyService productCurrencyService;
 
 
     @DoGet("/store")
-    public View storeGet(@RequestParam("lang") String language, @RequestParam(value = "limit", defaultValue = "10") int limit) {
+    public View storeGet(
+        @RequestParam("lang") String language,
+        @RequestParam(value = "limit", defaultValue = "10") int limit
+    ) {
         String defaultSortType = "ASC";
-        ProductCurrencyModel defaultCurrency = productCurrencyService
-            .getDefaultCurrency();
-        return View.builder()
+        ProductCurrencyModel defaultCurrency = productCurrencyService.getDefaultCurrency();
+        return View
+            .builder()
             .template("store")
             .addVariable("pageTitle", "store")
-            .addVariable(
-                "books",
+            .addVariable("books",
                 webProductControllerService.getWebProductPagination(
                     language,
                     DefaultProductFilter.builder().build(),
@@ -46,38 +49,40 @@ public class BookStoreController {
                     limit,
                     defaultCurrency.getId(),
                     defaultCurrency.getFormat()
-                )
-            )
-            .addVariable(
-                "categories",
-                productCategoryControllerService.getProductCategoryMenusByTypeAndStatuses(
+                ))
+            .addVariable("currencyId", defaultCurrency.getId())
+            .addVariable("categories",
+                webProductCategoryControllerService.getProductCategoryMenusByTypeAndStatuses(
                     BookStoreProductCategoryType.BOOK.toString(),
                     Collections.singletonList(ProductCategoryStatus.SHOW.toString())
-                )
-            )
+                ))
             .build();
     }
 
     @DoGet("/book-categories/{category-name}")
-    public View categoryGet(@RequestParam("lang") String language, @PathVariable("category-name") String categoryName,  @RequestParam(value = "limit", defaultValue = "10") int limit) {
+    public View categoryGet(
+        @RequestParam("lang") String language, @PathVariable("category-name") String categoryName,
+        @RequestParam(value = "limit", defaultValue = "10") int limit
+    ) {
         String defaultSortType = "ASC";
-        ProductCurrencyModel defaultCurrency = productCurrencyService
-            .getDefaultCurrency();
-        WebProductCategoryResponse category = productCategoryControllerService.getWebProductCategoryItemByNameAndTypeAndStatuses(
-            categoryName,
-            BookStoreProductCategoryType.BOOK.name(),
-            Collections.singletonList(ProductCategoryStatus.SHOW.name()),
-            language);
+        ProductCurrencyModel defaultCurrency = productCurrencyService.getDefaultCurrency();
+        WebProductCategoryResponse category = webProductCategoryControllerService
+            .getWebProductCategoryItemByNameAndTypeAndStatuses(
+                categoryName,
+                BookStoreProductCategoryType.BOOK.name(),
+                Collections.singletonList(ProductCategoryStatus.SHOW.name()),
+                language
+            );
         Long categoryId = null;
         if (category != null) {
             categoryId = category.getId();
 
         }
-        return View.builder()
+        return View
+            .builder()
             .template("store")
             .addVariable("pageTitle", categoryName)
-            .addVariable(
-                "books",
+            .addVariable("books",
                 webProductControllerService.getWebProductPagination(
                     language,
                     DefaultProductFilter.builder().inclusiveCategoryId(categoryId).build(),
@@ -89,15 +94,32 @@ public class BookStoreController {
                     limit,
                     defaultCurrency.getId(),
                     defaultCurrency.getFormat()
-                )
-            )
-            .addVariable(
-                "categories",
-                productCategoryControllerService.getProductCategoryMenusByTypeAndStatuses(
+                ))
+            .addVariable("categories",
+                webProductCategoryControllerService.getProductCategoryMenusByTypeAndStatuses(
                     BookStoreProductCategoryType.BOOK.toString(),
-                    Collections.singletonList(ProductCategoryStatus.SHOW.toString())
-                )
+                    Collections.singletonList(ProductCategoryStatus.SHOW.toString()))
             )
+            .addVariable("currencyId", defaultCurrency.getId())
             .build();
+    }
+
+    @DoGet("/api/v1/books/{bookId}")
+    public ProductResponse getProductById(
+        @PathVariable("bookId") Long bookId,
+        @RequestParam("currencyId") Long currencyId
+    ) {
+        if (currencyId == null) {
+            currencyId = productCurrencyService.getDefaultCurrency().getId();
+        }
+        if (bookId == null) {
+            return null;
+        }
+        ProductCurrencyModel currencyModel = productCurrencyService.getCurrencyById(currencyId);
+        return webProductControllerService.getProductById(
+            bookId,
+            currencyModel.getId(),
+            currencyModel.getFormat()
+        );
     }
 }
