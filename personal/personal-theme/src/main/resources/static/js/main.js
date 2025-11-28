f0f46f748f16b8116ed791b793d9426116adafca
Dung Ta Van <itprono3@gmail.com>
2025-11-28 13:38:12 +0700

    update

--- personal/personal-theme/src/main/resources/static/js/main.js
+++ personal/personal-theme/src/main/resources/static/js/main.js
@@ -6,7 +6,7 @@ const navMenuBtn = document.querySelector('.nav-menu-btn');
 const navCloseBtn = document.querySelector('.nav-close-btn');
 
 // navToggle function
-const navToggleFunc = function () { nav.classList.toggle('active'); }
+const navToggleFunc = function () { nav.classList.toggle('active'); };
 
 navMenuBtn.addEventListener('click', navToggleFunc);
 navCloseBtn.addEventListener('click', navToggleFunc);
@@ -15,13 +15,13 @@ navCloseBtn.addEventListener('click', navToggleFunc);
 const themeBtn = document.querySelectorAll('.theme-btn');
 
 for (let i = 0; i < themeBtn.length; i++) {
-  themeBtn[i].addEventListener('click', function () {
-    const themeElement = document.getElementById('theme-wrapper');
-    themeElement.classList.toggle('light-theme');
-    themeElement.classList.toggle('dark-theme');
-    for (let i = 0; i < themeBtn.length; i++) {
-      themeBtn[i].classList.toggle('light');
-      themeBtn[i].classList.toggle('dark');
-    }
-  })
+    themeBtn[i].addEventListener('click', function () {
+        const themeElement = document.getElementById('theme-wrapper');
+        themeElement.classList.toggle('light-theme');
+        themeElement.classList.toggle('dark-theme');
+        for (let i = 0; i < themeBtn.length; i++) {
+            themeBtn[i].classList.toggle('light');
+            themeBtn[i].classList.toggle('dark');
+        }
+    });
 }
