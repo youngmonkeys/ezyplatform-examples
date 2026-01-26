d4b7c31945663a46c62e64f9c066e5c451757d85
Dung Ta Van <itprono3@gmail.com>
2026-01-26 16:31:17 +0700

    fix checkstyle

--- book-store/book-store-theme/src/main/java/org/youngmonkeys/bookstore/web/controller/view/WebBookStorePageController.java
+++ book-store/book-store-theme/src/main/java/org/youngmonkeys/bookstore/web/controller/view/WebBookStorePageController.java
@@ -2,7 +2,6 @@ package org.youngmonkeys.bookstore.web.controller.view;
 
 import com.tvd12.ezyhttp.server.core.annotation.Controller;
 import org.youngmonkeys.ezyarticle.web.controller.view.PageController;
-import org.youngmonkeys.ezyarticle.web.controller.view.PostController;
 
 @Controller
 public class WebBookStorePageController extends PageController {}
